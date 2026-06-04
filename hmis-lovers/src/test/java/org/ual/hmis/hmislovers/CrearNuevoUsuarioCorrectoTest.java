dd9005ad4239eb7a6d7cb4b161048ac228b7ab97
Ángel Camacho Guirado <acg019@inlumine.ual.es>
2026-06-04 20:34:51 +0200

    little fix

--- hmis-lovers/src/test/java/org/ual/hmis/hmislovers/CrearNuevoUsuarioCorrectoTest.java
+++ hmis-lovers/src/test/java/org/ual/hmis/hmislovers/CrearNuevoUsuarioCorrectoTest.java
@@ -119,7 +119,7 @@ public class CrearNuevoUsuarioCorrectoTest {
     
 
     WebElement celdaUsuario = wait.until(ExpectedConditions.visibilityOfElementLocated(
-        By.xpath("//*[contains(., '" + nuevoUsuario + "')]")
+        By.xpath("//*[contains(text(), '" + nuevoUsuario + "')]")
     ));
     
     celdaUsuario.click();
